0fa5dcb52d5af3227b31169094e88f345cbc6db3
Navira Restu Ananda Tyana <restutyana99@gmail.com>
2024-03-01 00:35:04 +0700

    Update coba.php

--- coba.php
+++ coba.php
@@ -1,25 +1,64 @@
 <?php
 
-class Consultation {
-    private $mahasiswa;
-    private $kampus;
-    private $dosenWali;
+// Kelas Mahasiswa
+class Mahasiswa
+{
+    protected $nama;
 
-    public function __construct($mahasiswa, $kampus, $dosenWali) {
-        $this->mahasiswa = $mahasiswa;
-        $this->kampus = $kampus;
-        $this->dosenWali = $dosenWali;
+    public function __construct($nama)
+    {
+        $this->nama = $nama;
     }
 
-    public function displayConsultationInfo() {
-        echo "<p>Mahasiswa <strong>{$this->mahasiswa}</strong> sedang berkonsultasi dengan Dosen Wali <strong>{$this->dosenWali}</strong> mengurus KRS di lingkungan Kampus <strong>{$this->kampus}</strong>.</p>";
+    public function konsultasiKrs()
+    {
+        return $this->nama;
     }
 }
 
-// Membuat objek konsultasi
-$consultation = new Consultation("Navira", "Unissula", "FIDIA");
+// Kelas Kampus
+class Kampus
+{
+    protected $namaKampus;
 
-// Menampilkan informasi konsultasi
-$consultation->displayConsultationInfo();
+    public function __construct($namaKampus)
+    {
+        $this->namaKampus = $namaKampus;
+    }
 
-?>
+    public function getNamaKampus()
+    {
+        return $this->namaKampus;
+    }
+}
+
+// Kelas Dosen Wali sebagai turunan dari Mahasiswa dan Kampus
+class DosenWali extends Mahasiswa
+{
+    private $dosenWali;
+
+    public function __construct($nama, $dosenWali)
+    {
+        parent::__construct($nama);
+        $this->dosenWali = $dosenWali;
+    }
+
+    public function konsultasiKrs()
+    {
+        return parent::konsultasiKrs() . "<br><br>Dosen Pembimbing : {$this->dosenWali}.";
+    }
+}
+
+// Inisialisasi objek
+$namaMahasiswa = "Navira Restu Ananda Tyana";
+$namaKampus = "Islam Sultan Agung Semarang";
+$namaDosenWali = "Badieah Assegaf";
+
+$mahasiswa = new DosenWali($namaMahasiswa, $namaDosenWali);
+$kampus = new Kampus($namaKampus);
+
+// Tampilkan hasil di browser
+
+echo "<p>Nama : {$mahasiswa->konsultasiKrs()}</p>";
+echo "<p>Universitas : {$kampus->getNamaKampus()}.</p>";
+ ?>
