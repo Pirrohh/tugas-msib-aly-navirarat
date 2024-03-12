2c2c466d6f6525f5a3b5d6cfbe4afa422baa2ee0
Navira Restu Ananda Tyana <restutyana99@gmail.com>
2024-03-12 23:34:38 +0700

    sdxfcgvhv

---
+++ phpmvc/app/views/about/index.php
@@ -0,0 +1,6 @@
+<div class="container">
+    <h1 class="mt-4">BODY INDEX</h1>
+    <img src="<?= BASEURL; ?>/img/jpg-1.png" alt="Navira Restu Ananda Tyana">
+    <p>Halo,nama saya <?= $data['nama']; ?>, umur saya 21 Tahun , saya adalah seorang <?= $data['pekerjaan']; ?></p>
+
+</div>
